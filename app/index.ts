cb04e7e173a29149db657f800e58b677edf3e36d
Jordan Davidson <thatoneemail@gmail.com>
2018-09-06 09:47:21 -0600

    update index.ts to export required classes to create a custom loader

--- app/index.ts
+++ app/index.ts
@@ -3,3 +3,6 @@ export {
   DefaultPropertyDatabase as PropertyDatabase,
 } from './propertyDatabases/defaultPropertyDatabase';
 export { FilePropertyLoader } from './propertyLoaders/filePropertyLoader';
+export { PropertyLoader } from './propertyLoaders/propertyLoader';
+export { PropertySource } from './propertySources/propertySource';
+export { DefaultPropertySource } from './propertySources/defaultPropertySource';
